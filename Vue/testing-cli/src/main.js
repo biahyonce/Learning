926fd651325d7a875aca85e7d751743d0c5b5308
Bianca <biancacristina-silva@outlook.com>
2019-12-27 19:31:59 -0300

    Vue.js: Simple Routing From Scratch

--- Vue/testing-cli/src/main.js
+++ Vue/testing-cli/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Tasks from "./components/Tasks";
+import HelloWorld from "./components/HelloWorld";
+import NotFound from "./components/NotFound";
 
 Vue.config.productionTip = false
 
+const routes = {
+    '/': App,
+    '/test': Tasks,
+    '/hello': HelloWorld
+};
+
 new Vue({
-  render: h => h(App),
+    data: {
+        // In main.js, data only returns object, isn't function
+        // The data here is shared with the entire project, which is different from components
+        currentRoute: window.location.pathname
+    },
+
+    computed: {
+        currentComponent() { 
+            return routes[this.currentRoute] || NotFound;
+        }
+    },
+
+    render: function(h) {
+        return h(this.currentComponent);
+    }
 }).$mount('#app')
