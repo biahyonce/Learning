e2d4835d1e29ced276dd88614d7f5412450d1cc5
Bianca <biancacristina-silva@outlook.com>
2019-12-27 11:50:59 -0300

    Vue.js: Class and Style Bindings

--- Vue/testing-cli/src/App.vue
+++ Vue/testing-cli/src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-      <tasks/>
+      <div :class="classObject">{{value}}</div>
+      <div :style="{fontSize: value + 'px'}">{{value}}</div>
 
+      <button type="button" @click="value++">+</button>
+      <button type="button" @click="value--">-</button>
+      <button type="button" @click="error = !error">Flip error</button>
+      <button type="button" @click="warning = !warning">Flip warning</button>
   </div>
 </template>
 
 <script>
-import Tasks from "./components/Tasks";
 
 export default {
-  name: 'app',
-  components: {
-      Tasks
-  }
+    name: 'app',
+
+    components: {
+      //
+    },
+
+    data: function () {
+        return {
+            value: 1,
+            error: false,
+            warning: false
+        }
+    },
+
+    computed: {
+        classObject: function () {
+            return {
+                'red-error': this.value > 5,
+                'yellow-warning': this.warning
+            }
+        }
+    }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        font-size: 40px;
+        text-align: center;
+        padding-top: 100px;
+    }
+
+    .red-error {
+        background-color: darkred;
+    }
+
+    .yellow-warning {
+        text-decoration: underline;
+    }
 </style>
