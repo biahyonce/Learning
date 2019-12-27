a35474ce63efff948f366d615891f8fffbf7c5f9
Bianca <biancacristina-silva@outlook.com>
2019-12-27 11:14:26 -0300

    Vue.js: Introduction to Vue CLI and Vue UI Tool

---
+++ Vue/testing-cli/src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>Item 1</ul>
+        <ul>Item 2</ul>
+        <ul>Item 3</ul>
+        <ul>Item 4</ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tasks"
+    }
+</script>
+
+<style scoped>
+
+</style>
