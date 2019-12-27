7a2544176f30fac1aebd010f0e6e07fdcc44d875
Bianca <biancacristina-silva@outlook.com>
2019-12-27 20:50:41 -0300

    Vue.js: Vue Router

---
+++ Vue/testing-cli-2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/tasks">Tasks</router-link></li>
+      <li><router-link to="/user/123">User</router-link></li>
+    </ul>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/testing-cli-2/src/components/User.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+        {{ $route.params.id}} <!-- Same id described in main.js /user/:id-->
+    </h1>
+</template>
+
+<script>
+    export default {
+        name: "User"
+    }
+</script>
+
+<style scoped>
+
+</style>
