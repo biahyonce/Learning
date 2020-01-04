c604c5e73947853e2fbf8e68971a7801b8c6018a
Bianca <biancacristina-silva@outlook.com>
2020-01-04 15:16:38 -0300

    Vue.js: Burger Button

---
+++ Vue/awesome-website/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">
+        <burger></burger>
+    </div>
+</template>
+
+<script>
+import Burger from "./menu/Burger";
+export default {
+    name: 'HelloWorld',
+    components: {Burger},
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
