f62e3a92edd54f68d72a9002de366a1dd3f9ab34
Bianca <biancacristina-silva@outlook.com>
2020-01-04 16:00:33 -0300

    Vue.js: Sidebar + Burger

--- Vue/awesome-website/src/App.vue
+++ Vue/awesome-website/src/App.vue
@@ -1,27 +1,71 @@
 <template>
-  <div id="app">
-    <HelloWorld/>
-  </div>
+    <div id="app">
+        <nav class="main-nav">
+            <div class="logo">
+                my.company
+            </div>
+            <Burger></Burger>
+        </nav>
+
+        <Sidebar>
+            <ul class="sidebar-panel-nav">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </Sidebar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Burger from './components/menu/Burger.vue';
+import Sidebar from './components/menu/Sidebar.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Burger,
+        Sidebar
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html {
+        height: 100%;
+        overflow:hidden;
+    }
+
+    body {
+        border: 0; margin: 0; padding: 0;
+        font-family: 'Lato';
+        height: 100%;
+        background: rgb(101,31,87);
+        background: linear-gradient(45deg, rgba(101,31,87,1) 0%, rgba(225,113,87,1) 48%, rgba(249,248,113,1) 100%);
+    }
+
+    .logo {
+        align-self: center;
+        color: #fff;
+        font-weight: bold;
+        font-family: 'Lato';
+    }
+
+    .main-nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 0.5rem 0.8rem;
+    }
+
+    ul.sidebar-panel-nav {
+        list-style-type: none;
+    }
+
+    ul.sidebar-panel-nav > li > a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 1.5rem;
+        display: block;
+        padding-bottom: 0.5em;
+    }
 </style>
