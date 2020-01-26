9b115558b0da236f25f44d70a3784d6f99c5f67a
Bianca <biancacristina-silva@outlook.com>
2020-01-26 11:23:25 -0300

    Vue.js: Expandable Table (Vuetify)

--- Vue/vuetify/src/router/index.js
+++ Vue/vuetify/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Table from "../views/Table";
 import About from '../views/About'
+import ExpandableTable from "../views/ExpandableTable";
 
 Vue.use(VueRouter)
 
@@ -22,6 +23,12 @@ const routes = [
         path: '/table',
         name: 'table',
         component: Table
+    },
+
+    {
+        path: '/expandableTable',
+        name: 'Expandable Table',
+        component: ExpandableTable
     }
 ]
 
