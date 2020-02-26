b2d9e5441666b427b40c199740d938174ec82598
Bianca <biancacristina-silva@outlook.com>
2020-02-26 14:14:50 -0300

    Vue.js: Basic Tests (Jest + Vue-Test-Utils)

---
+++ Vue/vue-testing/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button class="btn" v-show="loggedIn">Logout</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                loggedIn: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
